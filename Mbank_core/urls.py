81b98252befc32a65398ee303dda361d55d17874
madaz1movsa <kutbidinovdanislan9@gmail.com>
2024-11-08 20:02:40 +0600

    "new"

--- Mbank_core/urls.py
+++ Mbank_core/urls.py
@@ -2,20 +2,23 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from users.views import ProfileListView, AddMoneyListView, TransactionListView, LoginListView, RegisterListView, \
-    ClickButtonView
+    ClickButtonView, MakeRegistrationView, MakeTransactionView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('profile/', ProfileListView.as_view(), name='profile'),
+    path('profile/', ProfileListView.as_view(), name='profile-url'),
 
     path('add-money/', AddMoneyListView.as_view(), name='add-money'),
     path('click-button/', ClickButtonView.as_view(), name='click-button-url'),
 
     path('transaction/', TransactionListView.as_view(), name='transactions'),
+    path('make-transaktion/', MakeTransactionView.as_view(), name='make-transaction-url'),
 
     path('login/', LoginListView.as_view(), name='login'),
-    path('registarstion/', RegisterListView.as_view(), name='registarstion'),
+
+    path('register/', RegisterListView.as_view(), name='register-url'),
+    path('registration', MakeRegistrationView.as_view(), name='registration-url'),
 
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-
+from django.http import Http404
+from django.shortcuts import render, redirect
+from django.db import IntegrityError, transaction
 from django.views.generic import TemplateView
 
+from django.contrib.auth import login
+from django.contrib import messages
+from django.views import View
 from users.models import CustomUser
+
+
+
 
 
 class ProfileListView(TemplateView):
@@ -31,7 +38,7 @@ class ClickButtonView(TemplateView):
 
     def get_context_data(self, **kwargs):
         current_user = self.request.user
-        current_user.balance += 1
+        current_user.balance += 1000
         current_user.save()
         context = {
             'current_user': current_user
@@ -41,8 +48,57 @@ class ClickButtonView(TemplateView):
 class TransactionListView(TemplateView):
     template_name = 'transaction-page.html'
 
+class MakeTransactionView(TemplateView):
+
+    def post(self, request, *args, **kwargs):
+        input_data = request.POST
+        phone_number = input_data['phone']
+        amount = int(input_data['amount'])
+
+        try:
+            receiver = CustomUser.objects.get(phone_number=phone_number)
+        except:
+            raise Http404
+
+        sender = request.user
+
+        with transaction.atomic():
+            receiver.balance += amount
+            sender.balance -= amount
+
+            receiver.save()
+            sender.save()
+
+        return redirect('profile-url')
+
 class LoginListView(TemplateView):
     template_name = 'login-page.html'
 
+
 class RegisterListView(TemplateView):
     template_name = 'registration-page.html'
+
+
+class MakeRegistrationView(View):
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+        password = data['password']
+        phone_number = data['phone']
+        username = data['name']
+
+        try:
+            user = CustomUser.objects.create_user(
+                first_name=username,
+                password=password,
+                phone_number=phone_number
+            )
+            user.save()
+
+            login(request, user)
+            return redirect('profile-url')
+        except IntegrityError:
+            messages.error(request, "Такой номер телефона уже зарегистрирован.")
+            return redirect('register-url')
+
+
+
